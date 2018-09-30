b9118d5d7fe97c9a8c4c30b6b46ac3773d3537c0
Sandino <paulosandino@hotmail.com>
2018-09-29 21:46:04 -0300

    Adicionada a lógica de atribuir doenças às pessoas e adicionado novos métodos relacionais das models

--- app/Controllers/Http/RelatorioController.js
+++ app/Controllers/Http/RelatorioController.js
@@ -28,16 +28,42 @@ class RelatorioController {
    */
   async store ({ params, request, response }) {
     
+    // Acha a pessoa na tabela de pessoas
     const pessoa = await Pessoa.find(params.pessoa_id)
 
+    // Cria um novo relatório
     const relatorio = new Relatorio()
+    
+    // Atribui ao relatório a flag de mutação recebida
     relatorio.mutacao = params.flag_mutacao
   
+    // Salva os relatórios com a relação hasMany
     await pessoa.relatorios().save(relatorio)
+
+    // Pega a quantidade de relatos que a pessoa recebeu
+    const numeroDeRelatos = await pessoa.relatorios().getCount()
+
+    // Quando a quantidade de relatos for igual a 3,
+    // uma comparação é feita para identificar o tipo de mutação
+    // e a pessoa é colocada em quarentena
+    if(numeroDeRelatos === 3){
+      let numeroDeDenteDeNeon = await pessoa.relatorios().where('mutacao', 1).getCount()
+      let numeroDeOlhoNaNuca = await pessoa.relatorios().where('mutacao', 2).getCount()
+
+      pessoa.localizacao = "Quarentena"
+
+      if(numeroDeDenteDeNeon > numeroDeOlhoNaNuca){
+        pessoa.mutacao = 1
+      }else{
+        pessoa.mutacao = 2
+      }
+
+      await pessoa.save()
+    }
 
     return response.status(201).json({
       mensagem: 'Relato criado com sucesso',
-      dados: relatorio
+      dados: await relatorio.pessoa().fetch()
     })
 
   }
@@ -47,18 +73,6 @@ class RelatorioController {
    * GET relatorios/:id
    */
   async show ({ params, request, response, view }) {
-
-    // const relatorio = await Relatorio.find(params.id)
-
-    // return response.status(200).json({
-    //   mensagem: "Relato",
-    //   dados: relatorio
-    // })
-
-    const relatorio = await Relatorio.find(params.id)
-
-    return relatorio.pessoa()
-
   }
 
   /**
